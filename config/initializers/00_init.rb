1a837d190dead8e050143e21d590a2edd50e8ce6
Avinasha Shastry <me@avinasha.com>
2012-07-23 18:47:31 +0700

    Using sb_config.yml

--- config/initializers/00_init.rb
+++ config/initializers/00_init.rb
@@ -1,2 +1,2 @@
 puts "Loading #{SupportBee::Importer.environment} Environment"
-APP_CONFIG = YAML.load_file("#{SupportBee::Importer.root.to_s}/config/config.yml")[SupportBee::Importer.environment.to_s]
+APP_CONFIG = YAML.load_file("#{SupportBee::Importer.root.to_s}/config/sb_config.yml")[SupportBee::Importer.environment.to_s]
